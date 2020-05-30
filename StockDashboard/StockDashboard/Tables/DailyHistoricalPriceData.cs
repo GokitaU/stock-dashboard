4c5dd971057708ae0af0fcc3b60af7b1bef3477e
Zgonz19 <gonzalo.zg.19@gmail.com>
2020-05-30 04:17:02 -0500

    data table

---
+++ StockDashboard/StockDashboard/Tables/DailyHistoricalPriceData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockDashboard.Tables
+{
+    public class DailyHistoricalPriceData
+    {
+        int SymbolId { get; set; }
+        DateTime MarketDate { get; set; }
+        decimal Open { get; set; }
+        decimal High { get; set; }
+        decimal Low { get; set; }
+        decimal Close { get; set; }
+        long Volume { get; set; }
+        decimal AdjustedClose { get; set; }
+    }
+}
