a794644ab5c2bb693da34b831f4f304646b99cbc
Zgonz19 <gonzalo.zg.19@gmail.com>
2020-05-30 04:13:49 -0500

    added tables

---
+++ StockDashboard/StockDashboard/Tables/DailyProcess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockDashboard.Tables
+{
+    public class DailyProcess
+    {
+        int SymbolId { get; set; }
+        DateTime LastestDate { get; set; }
+        string SuccessFlag { get; set; }
+    }
+}
---
+++ StockDashboard/StockDashboard/Tables/RootSymbolIndex.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockDashboard.Tables
+{
+    public class RootSymbolIndex
+    {
+        int Id { get; set; }
+        string Symbol { get; set; }
+        string CompanyName { get; set; }
+    }
+}
