52149a9138b633d3cc57070378e71206339b2899
Zgonz19 <gonzalo.zg.19@gmail.com>
2020-05-30 05:47:29 -0500

    for next time, build background service

---
+++ StockDashboard/StockDashboard/Services/DataServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockDashboard.Services
+{
+    public class DataServices
+    {
+        //https://medium.com/@daniel.sagita/backgroundservice-for-a-long-running-work-3debe8f8d25b
+    }
+}
