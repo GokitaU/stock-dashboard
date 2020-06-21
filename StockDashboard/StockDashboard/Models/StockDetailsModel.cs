b78d58281e43968745857db41ce0cf6f828d08ab
Zgonz19 <gonzalo.zg.19@gmail.com>
2020-06-21 09:51:08 -0500

    details

--- StockDashboard/StockDashboard/Models/StockDetailsModel.cs
+++ StockDashboard/StockDashboard/Models/StockDetailsModel.cs
@@ -7,5 +7,10 @@ namespace StockDashboard.Models
 {
     public class StockDetailsModel
     {
+        public decimal High52Weeks { get; set; }
+        public decimal Low52Weeks { get; set; }
+        public int TenDayVolume { get; set; }
+        public int FiveDayVolume { get; set; }
+
     }
 }
